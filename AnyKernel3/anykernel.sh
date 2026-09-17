5297a864920f41aa6cbf0536453377d7b9a3870a
Jubair Hosen <bmjubairdadu@users.noreply.github.com>
2026-09-17 10:33:08 -0600

    AnyKernel3 v2.3: aligned UI box, Android version check before flash

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -9,7 +9,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=DaisyForGaming v2.2 by JUBAIR HOSEN
+kernel.string=DaisyForGaming v2.3 by JUBAIR HOSEN
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -42,14 +42,33 @@ PATCH_VBMETA_FLAG=auto;
 ### DaisyForGaming stylish flashing screen
 ui_print " ";
 ui_print "  ############################################";
+ui_print "  #      D A I S Y  F O R  G A M I N G       #";
+ui_print "  #             by JUBAIR HOSEN              #";
 ui_print "  #                                          #";
-ui_print "  #     D A I S Y  F O R  G A M I N G        #";
-ui_print "  #                                          #";
-ui_print "  #       Developer : JUBAIR HOSEN           #";
-ui_print "  #       Device    : Mi A2 Lite (daisy)     #";
-ui_print "  #       Kernel    : 4.9.337 Gaming         #";
-ui_print "  #                                          #";
+ui_print "  #       Device  : Mi A2 Lite (daisy)       #";
+ui_print "  #         Kernel  : 4.9.337 Gaming         #";
 ui_print "  ############################################";
+ui_print " ";
+
+### Device + Android version check (runs after AK3 devicecheck)
+ui_print "  Checking device and Android version...";
+ui_print "  Device   : $(getprop ro.product.device 2>/dev/null)";
+ANDROID_VER=$(getprop ro.build.version.release 2>/dev/null);
+if [ ! "$ANDROID_VER" ]; then
+  ANDROID_VER=$(grep -m1 "^ro.build.version.release=" /system/build.prop 2>/dev/null | cut -d= -f2-);
+fi;
+ui_print "  Android  : $ANDROID_VER";
+case "$ANDROID_VER" in
+  9*|10*|11*|12*)
+    ui_print "  Supported version. Continuing...";
+    ;;
+  "")
+    ui_print "  Warning: version undetectable, continuing...";
+    ;;
+  *)
+    abort "  Android $ANDROID_VER is not supported by DaisyForGaming. Aborting...";
+    ;;
+esac;
 ui_print " ";
 
 ### AnyKernel install
@@ -66,8 +85,9 @@ write_boot;
 ## end install
 
 ui_print " ";
-ui_print "  ********************************************";
-ui_print "  *   Flashing done! Enjoy smooth gaming.    *";
-ui_print "  *   Developer : JUBAIR HOSEN               *";
-ui_print "  ********************************************";
+ui_print "  ############################################";
+ui_print "  #              Flashing done!              #";
+ui_print "  #           Enjoy smooth gaming.           #";
+ui_print "  #         Developer : JUBAIR HOSEN         #";
+ui_print "  ############################################";
 ui_print " ";
